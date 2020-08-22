aa89e0c8497e6b61b74c17da88dd329043fb05ef
tilnow <44685516+tilnow@users.noreply.github.com>
2020-08-22 20:41:12 +0300

    Update make_featured.rb

--- _plugins/make_featured.rb
+++ _plugins/make_featured.rb
@@ -10,7 +10,7 @@ module Make_featured
 
     def generate(site)
       td=Time.now.to_i / (3600*24)
-      l=site.collections['members'].docs.length()
+      l=site.collections['members'].docs.length()+1
       puts "we have ", l, " members" 
       site.data['featured']=td % l #day number from jan 1970 modulo number of actual members. alter can be fancier calculation
       site.data['build_date']=Date.today #today's date, without hours. ensures feed is daily. though feed has other feild that ensures that
