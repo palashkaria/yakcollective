1a5f2fee83ecb424d24131e7e834ce919486d64a
tilnow <44685516+tilnow@users.noreply.github.com>
2020-08-22 20:50:34 +0300

    see if/where member data goes

--- _plugins/make_featured.rb
+++ _plugins/make_featured.rb
@@ -11,7 +11,8 @@ module Make_featured
     def generate(site)
       td=Time.now.to_i / (3600*24)
       l=site.collections['members'].docs.length()+1
-      puts "we have ", l, " members" 
+      puts "we have ", l-1, " members in the collections" 
+      puts "we have ", site.data.members.length(), " members in data.members" 
       site.data['featured']=td % l #day number from jan 1970 modulo number of actual members. alter can be fancier calculation
       site.data['build_date']=Date.today #today's date, without hours. ensures feed is daily. though feed has other feild that ensures that
     end
